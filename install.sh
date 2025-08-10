d00bed3cfdadee1b2560176bef0f5c63aac99e5c
IanuReeves <66649143+IanuReeves@users.noreply.github.com>
2025-08-10 00:34:06 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
-sudo bash -c
-
-' mkdir -p /usr/local/bin /usr/local/etc
-
-curl -fsSL https://raw.githubusercontent.com/IanuReeves/Mac-Low-Power Script/main/Contents/lowpowermode_toggle.sh -o /usr/local/bin/lowpowermode_toggle.sh
- chmod +x /usr/local/bin/lowpowermode_toggle.sh
- 
- curl -fsSL https://raw.githubusercontent.com/IanuReeves/Mac-Low-Power-Script/main/Contents/lowpowermode.conf -o /usr/local/etc/lowpowermode.conf
- 
- curl -fsSL https://raw.githubusercontent.com/IanuReeves/Mac-Low-Power-Script/main/Contents/com.lowpowermode.toggle.plist -o /Library/LaunchDaemons/com.lowpowermode.toggle.plist
- chmod 644 /Library/LaunchDaemons/com.lowpowermode.toggle.plist
- 
- 
- launchctl unload /Library/LaunchDaemons/com.lowpowermode.toggle.plist 2>/dev/null || true launchctl load /Library/LaunchDaemons/com.lowpowermode.toggle.plist '
+sudo bash -c '
+mkdir -p /usr/local/bin /usr/local/etc
+curl -fsSL https://raw.githubusercontent.com/IanuReeves/Mac-Low-Power-Script/main/Contents/lowpowermode_toggle.sh -o /usr/local/bin/lowpowermode_toggle.sh
+chmod +x /usr/local/bin/lowpowermode_toggle.sh
+curl -fsSL https://raw.githubusercontent.com/IanuReeves/Mac-Low-Power-Script/main/Contents/lowpowermode.conf -o /usr/local/etc/lowpowermode.conf
+curl -fsSL https://raw.githubusercontent.com/IanuReeves/Mac-Low-Power-Script/main/Contents/com.lowpowermode.toggle.plist -o /Library/LaunchDaemons/com.lowpowermode.toggle.plist
+chmod 644 /Library/LaunchDaemons/com.lowpowermode.toggle.plist
+launchctl unload /Library/LaunchDaemons/com.lowpowermode.toggle.plist 2>/dev/null || true
+launchctl load /Library/LaunchDaemons/com.lowpowermode.toggle.plist
+'
